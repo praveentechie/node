8f26daab9997971a0660a52128853a5ec5fca7fa
praveentechie <praveen.arunachalam74@gmail.com>
2021-08-10 22:08:53 +0530

    Dockerize node app with PM2 1. Added minimal config for Docker, Express and PM2

---
+++ express-boilerplate/src/server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log(process.env.NODE_ENV);
+console.log(`Node server running on http://${HOST}:${PORT}`);
